ae41d200a43da2b564fe8dfae5fdeb8366ce0cee
Nick Thomas <nthomas@mozilla.com>
2010-11-27 23:17:23 +1300

    Hide pending Tegra jobs until they're back in action

--- buildapi/model/query.py
+++ buildapi/model/query.py
@@ -51,6 +51,8 @@ def GetBuilds(branch=None, type='pending', rev=None):
             ])
         q = q.where(and_(br.c.buildsetid==bs.c.id, bs.c.sourcestampid==ss.c.id))
         q = q.where(and_(br.c.claimed_at==0, br.c.complete==0))
+        # hide Tegras until they're actually processing requests
+        q = q.where(not_(br.c.buildername.startswith('Android Tegra')))
     elif type == 'running':
         q = select([b.c.id,
                     br.c.id.label('brid'),
